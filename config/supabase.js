9bcbad67bf3a556e5edcf6daba133fc1454f5c39
Gaspar <gas.toro@duocuc.cl>
2025-11-24 00:45:16 -0300

    fix supabase env key name

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// Usa el nombre correcto: SUPABASE_ANON_KEY
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+// Debe coincidir así:
+const supabase = createClient(process.env.SUPABASE_URL,
+   process.env.SUPABASE_ANON_KEY);
 
 module.exports = { supabase };
