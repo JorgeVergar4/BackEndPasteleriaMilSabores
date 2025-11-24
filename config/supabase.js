425d5fed2d822b602348fabde6bb204f8a3632ce
Gaspar <gas.toro@duocuc.cl>
2025-11-24 00:52:05 -0300

    fix supabase keys

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// Debe coincidir así:
-const supabase = createClient(process.env.SUPABASE_URL,
-   process.env.SUPABASE_ANON_KEY);
+// SOLO estas dos variables:
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 module.exports = { supabase };
